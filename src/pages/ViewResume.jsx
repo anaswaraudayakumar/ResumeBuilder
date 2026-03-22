daf64f08763e3aff8276d36bd110b372612a8ad1
unknown <anaswarakambaran@gmail.com>
2026-03-22 10:06:53 +0530

    ADD: view  Resume details

--- src/pages/ViewResume.jsx
+++ src/pages/ViewResume.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, { useEffect ,useState } from 'react'
 import { FaFileDownload } from "react-icons/fa";
 import { IoRefreshCircle } from "react-icons/io5";
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { FaFastBackward } from "react-icons/fa";
 import Preview from '../components/Preview';
 import Edit from '../components/Edit'
+import { getResumeAPI } from '../services/allResumeApiservices';
 function ViewResume() {
-  return (
+
+  const {id}=useParams()
+  console.log(id);
+  const [resumeData,setResumeData] =useState({})
+  console.log(resumeData);
+  useEffect(()=>{
+    getResumeDetails()
+  },[])
+
+  const getResumeDetails =async()=>{
+    if(id){
+      const result = await getResumeAPI(id)
+      console.log(result);
+      setResumeData(result.data)
+    }
+
+  }
+  
+  return (  
     <div className='container'>
       <div className='row my-3'>
         <div className="col-lg-2"></div>
         <div className="col-lg-8">
           <div className="d-flex justify-content-center align-items-center ">
             {/* download  */}
-            <button className='btn fs-2 me-2 '><FaFileDownload /></button>
+            <button classNam e='btn fs-2 me-2 '><FaFileDownload /></button>
             {/* edit  */}
             <Edit/>
 
@@ -24,7 +43,7 @@ function ViewResume() {
             <Link to={'/form'} className='btn fs-2 text-success me-2 '><FaFastBackward /></Link>
 
           </div>
-          <div><Preview/></div>
+          <div className='p-5'><Preview resumeData={resumeData}/></div>
         </div>
         <div className='col-lg-2'></div>
       </div>
--- src/services/allResumeApiservices.js
+++ src/services/allResumeApiservices.js
@@ -1,7 +1,13 @@
 import apiServices from "../api/apiService";
-//add resume api called by userinput
+//add resume api called by userinput when finish button clicked
 
 export const addResumeAPI = async (resumeData)=>{
    return  await apiServices("POST","/allResumes",resumeData)
+
+}
+
+//get resume api called by viewResume when page loaded (useEffect hook)
+export const getResumeAPI = async (id)=>{
+   return  await apiServices("GET",`/allResumes/${id}`,{})
 
 }
